f672452910b160af4a9414daab345de602d84adb
noulayfer <dqniejl@gmail.com>
2022-11-06 21:26:38 +0200

    LJ3-117

--- src/main/java/com/fedorenko/Main.java
+++ src/main/java/com/fedorenko/Main.java
@@ -1,26 +1,15 @@
 package com.fedorenko;
 
+import com.fedorenko.model.Car;
+import com.fedorenko.service.CarService;
+
 public class Main {
     public static void main(String[] args) {
-        // 1
-        String firstName = "Daniil";
-        String space = " ";
-        String lastName = "Fedorenko";
-        System.out.println(firstName + space + lastName);
-// 2
-        int i = 0;
-        int y = 5;
-        while (i <= 10){
-            System.out.println("step - " + i + ", value - " + y);
-            i++;
-            y += 2;
-        }
-// 3
-        for (i = 0; i <= 10; i++){
-            if (i == 3) continue;
-            if (i == 6) break;
-            System.out.println(i);
-        }
-
+        Car car1 = CarService.create();
+        Car car2 = CarService.create();
+        Car car3 = CarService.create();
+        CarService.print(car1);
+        CarService.print(car2);
+        CarService.print(car3);
     }
 }
---
+++ src/main/java/com/fedorenko/service/CarService.java
@@ -0,0 +1,38 @@
+package com.fedorenko.service;
+
+import com.fedorenko.model.Car;
+
+import java.util.Random;
+
+public class CarService {
+        static String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    public static Car create() {
+        String manufacturer = randomString();
+        String  engine = randomString();
+        String  color = randomString();
+        Car car = new Car(manufacturer, engine, color);
+        return car;
+    }
+
+    private static String randomString() {
+        String str = "";
+        Random random = new Random();
+        int[] randomArr = random.ints(random.ints(1, 3,10)
+                        .findAny().getAsInt(), 0, 25).toArray();
+        for (int i : randomArr) {
+            int j = random.nextInt();
+            if (j % 2 == 0) {
+                str += String.valueOf(alphabet.charAt(i)).toUpperCase();
+            } else {
+                str += alphabet.charAt(i);
+            }
+        }
+        return str;
+    }
+
+    public static void print(Car car) {
+        System.out.printf("Manufacturer: %s, Engine: %s, Color: %s, Count: %d, Price: %d%n",
+                car.getManufacturer(), car.getEngine(), car.getColor(), car.getCount(), car.getPrice());
+    }
+}
